6966ad52da56b007ce1e7481872fb3e6c1a106ec
Kush <kbanbah@yahoo.com>
2022-05-24 17:28:32 +0530

    Refactored Mainpage to stateful and filters

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -1,51 +1,69 @@
+// ignore_for_file: curly_braces_in_flow_control_structures
+
 import 'package:enum_to_string/enum_to_string.dart';
 import 'package:flutter/material.dart';
 import 'FileSetup.dart';
 import 'food.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  String test = "";
+  void runme() {
+    setState(() {
+      if (test == 'a')
+        test = '';
+      else
+        test = 'a';
+      print('test is ' + test);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     FileSetup.writeDefault();
 
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Hi'),
-          actions: const [
-            IconButton(
-                onPressed: null,
-                icon: Icon(
-                  Icons.add,
-                  color: Colors.white,
-                ))
-          ],
-        ),
-        body: Column(children: [
-          Container(child: const TextField(), padding: const EdgeInsets.all(8)),
-          Expanded(
-              child: FutureBuilder<List<Food>>(
-                  future: FileSetup.readFile(),
-                  builder: (context, AsyncSnapshot<List<Food>> curState) {
-                    if (curState.hasData)
-                      return FoodList(curState.data);
-                    else
-                      return const CircularProgressIndicator();
-                  }))
-        ]));
+    return Column(children: [
+      // Container(child: const TextField(), padding: const EdgeInsets.all(8)),
+      Container(
+          child: TextButton(
+            onPressed: runme,
+            child: Text(test),
+          ),
+          padding: const EdgeInsets.all(8)),
+      Expanded(
+          child: FutureBuilder<List<Food>>(
+              future: FileSetup.readFile(test),
+              builder: (context, AsyncSnapshot<List<Food>> curState) {
+                if (curState.hasData)
+                  return FoodList(curState.data);
+                else
+                  return const CircularProgressIndicator();
+              }))
+    ]);
   }
 }
 
 class FoodList extends StatefulWidget {
-  final List<Food>? li;
-  const FoodList(this.li, {Key? key}) : super(key: key);
+  List<Food>? li;
+  FoodList(this.li, {Key? key}) : super(key: key);
 
   @override
   State<FoodList> createState() => _FoodListState();
 }
 
 class _FoodListState extends State<FoodList> {
+  void update(List<Food> f) {
+    setState(() {
+      widget.li = f;
+    });
+  }
+
   Widget _buildrow(Food f) {
     Color r;
     switch (f.getState) {
