4e1e024055b365e24eef3606cb2af7dd06d07618
Kush <kbanbah@yahoo.com>
2022-05-25 21:05:05 +0530

    Added saving id

--- lib/food.dart
+++ lib/food.dart
@@ -1,4 +1,6 @@
-// ignore_for_file: constant_identifier_names
+// ignore_for_file: constant_identifier_names, curly_braces_in_flow_control_structures
+
+import 'dart:math';
 
 enum Type {
   All,
@@ -9,22 +11,25 @@ enum Type {
 enum state { red, green, yellow }
 
 class Food {
-  static int id = 0;
   final String _name;
   final Type _foodtype;
   final state _foodstate;
   late final int _myid;
+  static List<int> ids = [];
   Food(this._name, this._foodstate, this._foodtype) {
-    _myid = id;
-    print(id.toString() + ' my id is ' + _myid.toString());
-    id += 1;
+    var rand = Random();
+    int test = rand.nextInt(2000);
+    while (ids.contains(test)) test = rand.nextInt(2000);
+    _myid = test;
   }
 
-  Food.edit(this._name, this._foodstate, this._foodtype, this._myid);
+  Food.edit(this._name, this._foodstate, this._foodtype, this._myid) {
+    ids.add(_myid);
+  }
 
   String get getName => _name;
   Type get getType => _foodtype;
   state get getState => _foodstate;
-  String get StringVar => '$_name,$_foodstate,$_foodtype';
+  String get StringVar => '$_name,$_foodstate,$_foodtype,$getID';
   int get getID => _myid;
 }
